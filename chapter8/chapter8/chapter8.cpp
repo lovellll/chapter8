7e97faf8b33d3b3e81e10409d2ca1485006e3334
Geek song <503455810@qq.com>
2017-12-26 16:19:25 +0800

    8.3 2)

--- chapter8/chapter8/chapter8.cpp
+++ chapter8/chapter8/chapter8.cpp
@@ -22,15 +22,36 @@ public:
 		std::cout << "<" << m_x << "," << m_y << "," << m_z << ">\n";
 
 	}
+	bool isEqual(const Point3d &d)
+	{
+		if (m_x == d.m_x && m_y == d.m_y && m_z == d.m_z)
+			return true;
+		else
+			return false;
+	}
 };
 
 
 int main()
 {
-	Point3d point;
-	point.setValues(1.0, 2.0, 3.0);
+	Point3d point1;
+	point1.setValues(1.0, 2.0, 3.0);
 
-	point.print();
+	Point3d point2;
+	point2.setValues(1.0, 2.0, 3.0);
+
+	if (point1.isEqual(point2))
+		std::cout << "point1 and point2 are equal\n";
+	else
+		std::cout << "point1 and point2 are not equal\n";
+
+	Point3d point3;
+	point3.setValues(3.0, 4.0, 5.0);
+
+	if (point1.isEqual(point3))
+		std::cout << "point1 and point3 are equal\n";
+	else
+		std::cout << "point1 and point3 are not equal\n";
 
 	return 0;
 }
