d6dbbefb2d0b3c6247a97f70127f424b35aafbe2
Geek song <503455810@qq.com>
2017-12-28 16:05:38 +0800

    8.13

--- chapter8/chapter8/chapter8.cpp
+++ chapter8/chapter8/chapter8.cpp
@@ -3,41 +3,21 @@
 
 #include "stdafx.h"
 #include <iostream>
-#include <cassert>
-#include <string>
-#include <cstdint>
+#include "Point3d.h"
+#include "Vector3d.h"
 
-class RGBA
-{
-	std::uint8_t m_red;
-	std::uint8_t m_green;
-	std::uint8_t m_blue;
-	std::uint8_t m_alapha;
 
-public:
-	RGBA(std::uint8_t red=0,std::uint8_t green=0,std::uint8_t blue=0,std::uint8_t alapha=255):
-		m_red(red),m_green(green),m_blue(blue),m_alapha(alapha)
-	{
 
-	}
-
-	void print()
-	{
-		std::cout << "r=" << static_cast<int>(m_red) << " g=" << static_cast<int>(m_green) <<
-			" b=" << static_cast<int>(m_blue) << " a=" << static_cast<int>(m_alapha)<<"\n";
-	}
-};
 
 int main()
 {
-	RGBA teal(0, 127, 127);
-	teal.print();
-	
-	RGBA def;
-	def.print();
+	Point3d p(1.0, 2.0, 3.0);
+	Vector3d v(2.0, 2.0, -3.0);
 
+	p.print();
+	p.moveByVector(v);
+	p.print();
 
 	return 0;
-
 }
 
